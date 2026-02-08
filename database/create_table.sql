3818037f4f08c475f3207af42aea878b4816233a
karthik <karthikca002@gmail.com>
2026-02-08 12:58:52 +0530

    Create create_table.sql

---
+++ database/create_table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS sales (
+    order_id INTEGER,
+    order_date TEXT,
+    customer TEXT,
+    product TEXT,
+    category TEXT,
+    quantity INTEGER,
+    price INTEGER,
+    region TEXT
+);
